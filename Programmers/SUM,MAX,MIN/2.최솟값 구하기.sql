e68687431ae23146cf79a451016146af550ee76e
yh3674git config --global user.name <yoonhk3674@gmail.com>
2022-08-31 19:44:03 +0900

    2022_08_31

---
+++ Programmers/SUM,MAX,MIN/2.최솟값 구하기.sql
@@ -0,0 +1,5 @@
+-- 동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+SELECT DATETIME AS '시간' FROM ANIMAL_INS
+
+-- 가장 먼저 들어온 동물은 Jack이고, Jack은 2013-10-14 15:38:00에 들어왔습니다.
+WHERE DATETIME = (SELECT MIN(DATETIME) FROM ANIMAL_INS)

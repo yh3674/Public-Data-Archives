3605aa0f9da2dc8f60ba2fee25189f93eb5ca2f2
yh3674git config --global user.name <yoonhk3674@gmail.com>
2022-09-01 16:46:40 +0900

    Lecture_Practice

--- Programmers/GROUP BY/INNER JOIN_Practice.sql
+++ Programmers/GROUP BY/INNER JOIN_Practice.sql
@@ -5,6 +5,12 @@
 
 -- [Attempted Approach] 
 
+--SELECT Orders.OrderID,Shippers.ShipperID, Shippers.Phone
+--FROM Orders
+--INNER JOIN Shippers
+--ON Order.OrderID = Shippers.ShipperID;
+
+-- [Correct Approach] 
 SELECT Orders.OrderID,Shippers.ShipperID, Shippers.Phone
 FROM Orders
 INNER JOIN Shippers

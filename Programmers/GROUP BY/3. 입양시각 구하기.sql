e129d941f3e13c42656cd0ab8f58f70f535631e5
yh3674git config --global user.name <yoonhk3674@gmail.com>
2022-09-01 15:45:00 +0900

    Lecture_Practice

---
+++ Programmers/GROUP BY/3. 입양시각 구하기.sql
@@ -0,0 +1,13 @@
+-- [Intedned Approach]
+
+-- SELECT HOUR(DATETIME), COUNT(DATETIME) AS Count
+-- FROM ANIMAL_OUTS
+-- ORDER BY DATETIME;       ===> HOUR(DATETIME)	Count
+ --                                   11	    100
+
+ -- [Corrected Approach]
+SELECT HOUR(DATETIME) as Hour, COUNT(DATETIME)
+FROM ANIMAL_OUTS
+WHERE Hour(DATETIME) BETWEEN 9 AND 19
+GROUP BY Hour
+ORDER BY HOUR ASC;

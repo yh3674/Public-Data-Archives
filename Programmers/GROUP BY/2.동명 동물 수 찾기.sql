7615d4f1aa771a673bd80b6a34c19669c8f526c2
yh3674git config --global user.name <yoonhk3674@gmail.com>
2022-09-01 14:12:23 +0900

    2022-09-01

--- Programmers/GROUP BY/2.동명 동물 수 찾기.sql
+++ Programmers/GROUP BY/2.동명 동물 수 찾기.sql
@@ -12,7 +12,6 @@
 --ORDER BY NAME;
 
 -- [Corrected Approach with Reference] [Alias]
--- 동물 보호소에 들어온 동물 이름 중 두 번 이상 쓰인 이름과 해당 이름이 쓰인 횟수를 조회하는 SQL문을 작성해주세요. 이때 결과는 이름이 없는 동물은 집계에서 제외하며, 결과는 이름 순으로 조회
 
 SELECT NAME, CTable.COUNT
     FROM(

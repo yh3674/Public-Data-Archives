e3f8a19932d28c1c0d52cdb7fe1d9477499bca8b
yh3674git config --global user.name <yoonhk3674@gmail.com>
2022-09-01 16:44:11 +0900

    Lecture_Practice

--- Programmers/SELECT/1.모든 레코드 조회하기.sql
+++ Programmers/SELECT/1.모든 레코드 조회하기.sql
@@ -3,3 +3,12 @@
 -- 동물 보호소에 들어온 모든 동물의 정보를 ANIMAL_ID순으로 조회하는 문을 작성
 SELECT * FROM ANIMAL_INS -- ANIMAL_INS 모든 항목 조회
 ORDER BY ANIMAL_ID  -- ANIMAL_ID를 내림차순으로 조회
+
+SELECT Orders.OrderID,Shippers.ShipperID, Shippers.Phone
+FROM Orders
+INNER JOIN
+
+SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
+FROM Orders
+INNER JOIN Customers
+ON Orders.CustomerID=Customers.CustomerID;

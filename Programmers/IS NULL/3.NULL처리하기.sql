d5f261cbcc5bdd3566b8b8765086049de5d92afb
yh3674git config --global user.name <yoonhk3674@gmail.com>
2022-09-03 18:00:35 +0900

    Lecture_Practice

--- Programmers/IS NULL/3.NULL처리하기.sql
+++ Programmers/IS NULL/3.NULL처리하기.sql
@@ -5,7 +5,10 @@
 --WHERE NAME IS NULL AS 'No Name'
 
 -- [Corrected Approach]
-SELECT animal_type,     
-    if(name is null,"No name",name), sex_upon_intake
-from animal_ins
-order by animal_id;
+
+
+
+SELECT ANIMAL-TYPE,
+    IF(NAME IS NULL, "No Name", NAME),SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
